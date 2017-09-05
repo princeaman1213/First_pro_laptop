3951ce355040cf0679fbbd802a778b4e7253eaa6
princeaman1213 <princeaman1213@gmail.com>
2017-09-05 15:47:42 +0530

    second commit

---
+++ errors_signature.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type errortest struct {
+	lat,long string
+	err error
+}
+
+func (rec errortest) Error() string{
+	return fmt.Sprintf("error occured %v %v %v",rec.lat,rec.long,rec.err)
+}
+
+func main() {
+	_,err:=sqrt(-12)
+	if err!=nil{
+		fmt.Println(err)
+	}
+
+}
+
+func sqrt(n int) (int , error){
+	if n<0{
+		err1:=fmt.Errorf("sq root of neg num. %v",n)
+		return 0,errortest{"30 N","43 E",err1}
+	}else {
+		return 0,nil
+	}
+
+}
