ff0b1b7017d02868af85288ff22628237bd03034
princeaman1213 <princeaman1213@gmail.com>
2017-09-05 17:01:26 +0530

    second commit

--- errors_signature.go
+++ errors_signature.go
@@ -17,12 +17,13 @@ func main() {
 		fmt.Println(err)
 	}
 
+	fmt.Printf("%T \n %T",fmt.Errorf("sq root of neg num. %v"),fmt.Sprintf("sq root of neg num. %v"))
 }
 
 func sqrt(n int) (int , error){
 	if n<0{
 		err1:=fmt.Errorf("sq root of neg num. %v",n)
-		return 0,errortest{"30 N","43 E",err1}
+		return 0,&errortest{"30 N","43 E",err1}
 	}else {
 		return 0,nil
 	}
