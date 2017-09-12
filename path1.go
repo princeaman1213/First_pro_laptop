a73f8c8c34b7c6fb98209c304dbf673d724c2511
princeaman1213 <princeaman1213@gmail.com>
2017-09-12 18:03:39 +0530

    12/9/17

--- path1.go
+++ path1.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"sync"
+	"time"
 )
 
 type person struct {
@@ -19,13 +20,14 @@ func main() {
     var a,b,c person
 
 		arrx:=[]float64{0,185,212,265,273,279,292,367,392,426,434,437,477,491,499}
-		arry:=[]float64{0,78,99,146,188,56,243,69,321,324,115,132,364,225,121}    //coordinates of person 1 on route 1
+		arry:=[]float64{0,448,99,146,188,56,243,69,321,324,115,132,364,225,121}    //coordinates of person 1 on route 1
 
 		arrx1:=[]float64{0,123,156,190,223,235,278,302,306,387,404,458,480,499}
 		arry1:=[]float64{0,35,63,355,255,144,35,366,465,234,134,234,465,324,211}    //coordinates of person 1 on route 2
 
 		arrx2:=[]float64{0,142,156,178,234,278,333,387,390,406,453,487,499}
-		arry2:=[]float64{0,24,57,167,388,299,120,268,444,100,98,167,22,110}    //coordinates of person 1 on route 3
+		arry2:=[]float64{0,24,57,167,388,299,120,268,444,100,98,167,22,110}
+		fmt.Println(time.Now())//coordinates of person 1 on route 3
 
 		go func(){
 			a=calc(1,arrx,arry)
